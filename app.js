f0c7a06b74655b026675668f63d8b411b51aeab6
DeanIsseyegh <issdea@gmail.com>
2014-05-31 16:03:50 +0100

    First post request test

--- app.js
+++ app.js
@@ -35,3 +35,30 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.post('/savesurvey', function(req, res){
+	//  console.log("req obj is: ");
+	//  console.dir(req.body);
+
+/*	  var request = req.body;
+
+	  mongo.Db.connect(mongo_uri, function (err, db) {
+		db.collection('products', function(er, collection) {
+			collection.insert(request, {safe: true}, function(er,rs) {
+				if (rs) {
+					res.send("Inserted Successfully");
+					console.log('Inserted into products table!' + rs);
+				} else  {
+					if (err) {
+						console.log('Error: ' + er);
+						res.send(err);
+					} else {
+						console.log("No response from database");
+						res.send("No response from insertion query");
+					}
+				}
+			}); //end of collection.insert
+		}); //end of db.collection 
+	  }); //end of mongo.Db.connect
+*/	  
+	}); //end of post /add
